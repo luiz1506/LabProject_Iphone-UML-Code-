f1b619aea6e85c2f4f1fcb06596983b955d56add
Luiz G. <66616028+luiz1506@users.noreply.github.com>
2025-04-24 21:56:48 -0300

    Create InnerAparelhoTelefonico.java

---
+++ InnerAparelhoTelefonico.java
@@ -0,0 +1,16 @@
+package com.example.demo.Iphone;
+
+
+    public interface InnerAparelhoTelefonico {
+
+        void ligar (String numero);
+
+        void atender();
+
+        void iniciarCorreioVoz();
+
+    
+        
+    }
+    
+
