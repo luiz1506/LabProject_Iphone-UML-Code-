9ec7e95f9815f91ac77a535bc06dcb006943236b
Luiz G. <66616028+luiz1506@users.noreply.github.com>
2025-04-24 21:57:58 -0300

    Create InnerReprodutorMusical.java

---
+++ InnerReprodutorMusical.java
@@ -0,0 +1,15 @@
+package com.example.demo.Iphone;
+
+
+    public interface InnerReprodutorMusical {
+
+        void selecionarMusica (String musica);
+
+        void tocar();
+
+        void pausar();
+
+    
+        
+    }
+    
